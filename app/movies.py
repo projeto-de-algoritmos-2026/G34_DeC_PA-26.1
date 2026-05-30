b78ecd4be3f995bae6a17e094ad4b9a0b9a812a3
Ruan-Carvalho <ruansobreira11@gmail.com>
2026-05-29 22:09:24 -0300

    feat: implementar movies.py com a lista de 10 filmes

---
+++ app/movies.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Lista hardcoded contendo exatamente 10 filmes selecionados para a avaliação de preferências.
+MOVIES = [
+    "O Poderoso Chefão",
+    "Interestelar",
+    "A Origem",
+    "Clube da Luta",
+    "Pulp Fiction",
+    "Matrix",
+    "O Senhor dos Anéis: O Retorno do Rei",
+    "O Show de Truman",
+    "A Viagem de Chihiro",
+    "Como Estrelas na Terra"
+]
